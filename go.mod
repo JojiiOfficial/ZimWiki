07fdaa7bc68907da22470317d402bffe91f121b8
jojii <jojii@gmx.net>
2020-07-02 20:22:43 +0200

    add mod

---
+++ go.mod
@@ -0,0 +1,8 @@
+module git.jojii.de/jojii/zimserver
+
+go 1.14
+
+require (
+	github.com/tim-st/go-zim v0.0.1
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+
+	zim "github.com/tim-st/go-zim"
+)
+
+func main() {
+	file, err := zim.Open("archlinux_wiki.zim")
+	defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(file.License())
+}
