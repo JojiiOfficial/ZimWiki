bcc0653c3335298e55729489c0a9b6ca20c2acb4
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-06-03 19:38:26 +0200

    Do not search if the search field is empty or contains only spaces

--- html/static/assets/js/script.js
+++ html/static/assets/js/script.js
@@ -1,23 +1,22 @@
 function enterSearch(ele){
-	if (event.key === 'Enter'){
-		search(ele.value)
+	if (event.key === 'Enter') {
+		search(ele.value);
 	}
 }
 
 function search(text) {
-	searchForm = document.getElementById("searchform")
-	queryData = document.getElementById("sQuery")
-
+	let searchForm = document.getElementById("searchform");
+	let queryData = document.getElementById("sQuery");
 	document.querySelector("#searchform").setAttribute("action","/search/"+wiki+"/");
-
-	queryData.value = text
-
-	searchForm.submit()
+	queryData.value = text;
+	if (text.replace(/\s/g, "") != "") {
+		searchForm.submit();
+	}
 }
 
 // For all those vim lovers
 document.addEventListener('keyup', (e) => {
-    if (e.keyCode === 191){
-      document.getElementById("searchtxt").focus()
+    if (e.keyCode === 191) {
+      document.getElementById("searchtxt").focus();
     }
 });
