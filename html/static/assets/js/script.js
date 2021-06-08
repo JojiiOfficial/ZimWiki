7404c6730cbab0d4edd8dfc434048f2eb92fed6d
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-06-08 16:38:43 +0200

    Update script.js

--- html/static/assets/js/script.js
+++ html/static/assets/js/script.js
@@ -2,20 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector(".no-js").style.visibility="visible";
 });
 
-// For all those vim lovers
-document.addEventListener('keyup', (e) => {
-    if (e.keyCode === 191) {
-      document.getElementById("sQuery").focus();
-    }
-});
-
 if (typeof queryText !== 'undefined') {
 	document.getElementById("sQuery").value = queryText;
 }
-
 
 if (wiki == "-") {
 	document.getElementById("sQuery").placeholder = "Search anywhere";
 } else {
 	document.getElementById("sQuery").placeholder = "Search in this wiki";
 }
+
+// For all those vim lovers
+document.addEventListener('keyup', (e) => {
+    if (e.keyCode === 191) {
+      document.getElementById("sQuery").focus();
+    }
+});
