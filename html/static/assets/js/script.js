3643f196d7aa32ed9cdbee7e65ea159804989783
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-06-08 12:56:42 +0200

    Search without using JavaScript

--- html/static/assets/js/script.js
+++ html/static/assets/js/script.js
@@ -2,26 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector(".no-js").style.visibility="visible";
 });
 
-function enterSearch(ele){
-	if (event.key === 'Enter') {
-		search(ele.value);
-	}
-}
-
-function search(text) {
-	let searchForm = document.getElementById("searchform");
-	let queryData = document.getElementById("sQuery");
-	document.querySelector("#searchform").setAttribute("action","/search/"+wiki+"/");
-	queryData.value = text;
-	if (text.replace(/\s/g, "") != "") {
-		searchForm.submit();
-	}
-}
-
 // For all those vim lovers
 document.addEventListener('keyup', (e) => {
     if (e.keyCode === 191) {
-      document.getElementById("searchtxt").focus();
+      document.getElementById("sQuery").focus();
     }
 });
 
