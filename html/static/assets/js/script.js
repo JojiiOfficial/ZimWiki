833440cc8475270ec3ad80b0948a96f73d98daf3
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2021-08-16 23:41:49 +0200

    Update script.js

--- html/static/assets/js/script.js
+++ html/static/assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 	document.querySelector(".no-js").style.visibility = "visible";
 });
 
-if (typeof queryText !== 'undefined') {
+if (typeof queryText !== "undefined") {
   document.getElementById("sQuery").value = queryText;
 }
 
@@ -31,7 +31,7 @@ window.onpageshow = function() {
 }
 
 // For all those vim lovers
-document.addEventListener('keyup', (e) => {
+document.addEventListener("keyup", (e) => {
 	if (e.keyCode === 191) {
 		document.getElementById("sQuery").focus();
 	}
